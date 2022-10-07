12cd93ee5202aec01e731196761483103f04067e
IBalitskiy <IBalitskiy@inno.tech>
2022-10-07 20:36:55 +0300

    init

---
+++ src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/dao/DataGeneratorDao.scala
@@ -0,0 +1,120 @@
+package ru.vtb.uasp.streaming.mdm.enrichment.itest.dao
+
+import ru.vtb.uasp.common.dto.UaspDto
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.common.FooCounter
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.SendRateScenarioBuilder
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.SendRateScenarioBuilder.timeNow
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+object DataGeneratorDao {
+
+
+  def generateMsgFields(id: Int): Map[String, Any] = {
+    val random = new scala.util.Random
+    val fooCounter = new FooCounter(id)
+    val USER_START_ID_NUMBER = (Math.random() * 100000).toInt * 10000 + fooCounter.inc() * random.nextInt(1000)
+
+    Map(
+      "transaction_datetime" -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime),
+      "processing_datetime" -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime),
+      "processing_effectivedate" -> new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime),
+      "op_id" -> random.nextInt(999999),
+      "audit_ref_authcode" -> random.nextInt(999999),
+      "audit_ref_rrn" -> random.alphanumeric.take(12).mkString.toUpperCase,
+      "audit_ref_srn" -> random.alphanumeric.take(12).mkString.toUpperCase,
+      "local_user_id" -> (fooCounter.inc() + USER_START_ID_NUMBER).toString,
+      "balance_change" -> random.nextInt(9999)
+    )
+  }
+
+  def generateWay4(userIdLocal: String, idMsg: String): UaspDto = {
+    val random = new scala.util.Random
+    UaspDto(
+      id = userIdLocal,
+      dataInt = Map(      ),
+      dataLong = Map(
+      ),
+      dataFloat = Map(),
+      dataDouble = Map(),
+      dataDecimal = Map(),
+      dataString = Map(
+        "tcmt_account_num" -> userIdLocal,
+      ),
+      dataBoolean = Map(),
+      uuid = java.util.UUID.randomUUID().toString,
+      process_timestamp = Calendar.getInstance().getTimeInMillis
+    )
+  }
+
+
+  def generateCrossLinkMdm(userIdLocal: String, idGloabal: String): UaspDto = {
+    UaspDto(
+      userIdLocal,
+      Map("period" -> 12),
+      Map(),
+      Map(),
+      Map(),
+      Map(
+        "product_rate"-> BigDecimal(1)
+      ),
+      Map(
+        "product_nm" -> "product_nm",
+/*        "mask_card_number" -> "MASK_CARD_NUMBER",
+        "customer_id" -> "customer_id",
+        "source_system_cd" -> "source_system_cd",
+        "pos_flg" -> "pos_flg",
+        "account_num" -> "account_num",
+        "is_virtual_card_flg" -> "is_virtual_card_flg",
+        "card_expiration_dt" -> "card_expiration_dt",
+        "payment_system_desc" -> "payment_system_desc",
+        "card_type_cd" -> "card_type_cd",
+        "salary_serv_pack_flg" -> "salary_serv_pack_flg",
+        "salary_project_flg" -> "salary_project_flg",
+        "salary_account_scheme_flg" -> "salary_account_scheme_flg",
+        "salary_card_type_flg" -> "salary_card_type_flg",
+        "salary_card_type_flg" -> "salary_card_type_flg",
+        "contract_card_type_cd" -> "contract_card_type_cd",
+        "mdm_id" -> "mdm_id",*/
+      ),
+      Map(),
+      java.util.UUID.randomUUID().toString,
+      Calendar.getInstance().getTimeInMillis
+    )
+  }
+
+  def generateMortgageMdm(idGloabal: String): UaspDto = {
+    UaspDto(
+      idGloabal,
+      Map(),
+      Map(),
+      Map(),
+      Map(),
+      Map(),
+      Map(
+        "MASK_CARD_NUMBER"-> "MASK_CARD_NUMBER",
+        "customer_id"-> "customer_id",
+        "source_system_cd"-> "source_system_cd",
+        "pos_flg"-> "pos_flg",
+        "account_num"-> "account_num",
+        "is_virtual_card_flg"-> "is_virtual_card_flg",
+        "card_expiration_dt"-> "card_expiration_dt",
+        "payment_system_desc"-> "payment_system_desc",
+        "card_type_cd"-> "card_type_cd",
+        "salary_serv_pack_flg"-> "salary_serv_pack_flg",
+        "salary_project_flg"-> "salary_project_flg",
+        "salary_account_scheme_flg"-> "salary_account_scheme_flg",
+        "salary_card_type_flg"-> "salary_card_type_flg",
+        "salary_card_type_flg"-> "salary_card_type_flg",
+        "contract_card_type_cd"-> "contract_card_type_cd",
+        "MASK_CARD_NUMBER"-> "MASK_CARD_NUMBER",
+
+      ),
+      Map("is_mortgage" -> (idGloabal.map(a => a.toInt).sum % 2 == 0)),
+      java.util.UUID.randomUUID().toString,
+      Calendar.getInstance().getTimeInMillis
+    )
+  }
+
+}
---
+++ src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/utils/ConfigUtil.scala
@@ -0,0 +1,55 @@
+package ru.vtb.uasp.streaming.mdm.enrichment.itest.utils
+
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.entity.Config
+
+object ConfigUtil {
+
+  val groupId = 24
+
+  def getConf(sysEnv: Map[String, String]): Config = {
+    val config: Config = Config(
+            topicInWay4 = sysEnv.getOrElse("topicInWay4", "dev_bevents__realtime__input_converter__prof__transactions__uaspdto"),
+            topicInCrossLinkMdm = sysEnv.getOrElse("topicInCrossLinkMdm", "dev_rto_batch_ca_deposit_account_case_71_uaspdto"),
+//      topicInWay4 = sysEnv.getOrElse("topicInWay4", "A_CASE_71_HA_IN"),
+//      topicInCrossLinkMdm = sysEnv.getOrElse("topicInCrossLinkMdm", "A_CASE_71_CA_IN"),
+
+
+      topicInMortgage = sysEnv.getOrElse("topicInMortgage", "dev_bevents_card_agreement_enrich_out_uaspdto"),
+      topicOutMortgage = sysEnv.getOrElse("topicOutMortgage", "dev_bevents_card_agreement_enrich_out_uaspdto"),
+      topicOutCrossLinkMdmStatus = sysEnv.getOrElse("topicOutCrossLinkMdmStatus", "dev_bevents_card_agreement_enrich_out_uaspdto"),
+      topicOutEnrichmentWay4 = sysEnv.getOrElse("topicOutEnrichmentWay4", "dev_ivr__uasp_realtime__mdm_enrichment__uaspdto"),
+      topicDLQ = sysEnv.getOrElse("topicDLQ", "dev_bevents_card_agreement_enrich_out_uaspdto"),
+      topicInRate = sysEnv.getOrElse("topicInRate", "rate_in"),
+      topicOutRate = sysEnv.getOrElse("topicInRate", "rate_out"),
+      bootstrapServers = sysEnv.getOrElse("bootstrapServers", "d5uasp-kfc001lk.corp.dev.vtb:9092,d5uasp-kfc002lk.corp.dev.vtb:9092,d5uasp-kfc003lk.corp.dev.vtb:9092,d5uasp-kfc004lk.corp.dev.vtb:9092,d5uasp-kfc005lk.corp.dev.vtb:9092,d5uasp-kfc006lk.corp.dev.vtb:9092"),
+      //      bootstrapServers = sysEnv.getOrElse("bootstrapServers", "172.19.128.77:9092,172.19.128.77:9091"),
+      groupIdCrossLinkStatusMdm = sysEnv.getOrElse("groupIdCrossLinkStatusMdm", "groupIdCrossLinkStatusMdm_ksu"),
+      groupIdMortgageMdm = sysEnv.getOrElse("groupIdMortgageMdm", "groupIdMortgageMdm-" + groupId),
+      groupIdWay4 = sysEnv.getOrElse("groupIdWay4", "groupIdWay4_ksu-" + groupId),
+      groupIdDlq = sysEnv.getOrElse("groupIdDlq", "groupIdDlq_ksu-" + groupId),
+
+      sslTruststoreLocation = sysEnv.getOrElse("sslTruststoreLocation", "C:\\Work\\SSL\\new_APD75-None-kafka-d5-client-uasp-truststore.pfx"),
+      sslTruststorePassword = sysEnv.getOrElse("sslTruststorePassword", "p9HrxNIXS4ekSBKKD0Dh<zL7a2!"),
+      sslKeystoreLocation = sysEnv.getOrElse("sslKeystoreLocation", "C:\\Work\\SSL\\new_APD75-None-kafka-d5-client-uasp.pfx"),
+      sslKeystorePassword = sysEnv.getOrElse("sslKeystorePassword", "p9HrxNIXS4ekSBKKD0Dh<zL7a2!"),
+      sslKeyPassword = sysEnv.getOrElse("sslKeyPassword", "p9HrxNIXS4ekSBKKD0Dh<zL7a2!"),
+
+      /**
+       * sysEnv.getOrElse("sslTruststoreLocation", "kafka-trust.pfx"),
+       * sysEnv.getOrElse("sslTruststorePassword", ""),
+       * sysEnv.getOrElse("sslKeystoreLocation", "APD00.13.01-USBP-kafka-cluster-uasp.pfx"),
+       * sysEnv.getOrElse("sslKeystorePassword", ""),
+       * sysEnv.getOrElse("sslKeyPassword", ""), */
+
+      nameStateDayHourAggregates = sys.env.getOrElse("nameStateDayHourAggregates", "DayHourAggregates"),
+      nameStateMaxDurations = sys.env.getOrElse("nameStateMaxDurations", "MaxDurations"),
+      nameStateLastTransaction = sys.env.getOrElse("nameStateLastTransaction", "LastTransaction"),
+      enablePrefix = sys.env.getOrElse("enablePrefix", "true"),
+      prefix = sys.env.getOrElse("prefix", "EnItest!-"),
+      rateList = sys.env.getOrElse("prefix", "USD,EUR,RUB").split(",").toList,
+      dateDiapason = sys.env.getOrElse("prefix", "1").toInt,
+    )
+
+    config
+  }
+}

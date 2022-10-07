b50a4766da243b83aa683a037b0a786188064f72
IBalitskiy <IBalitskiy@inno.tech>
2022-10-07 20:45:39 +0300

    init

--- src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/scenario/SendMdmCrossLinksScenarioBuilder.scala
+++ src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/scenario/SendMdmCrossLinksScenarioBuilder.scala
@@ -6,25 +6,21 @@ import io.gatling.core.structure.ScenarioBuilder
 import ru.vtb.uasp.common.dto.UaspDto
 import ru.vtb.uasp.common.utils.avro.AvroSerializeUtil
 import ru.vtb.uasp.mdm.enrichment.dao.UaspDtoPredef
-import ru.vtb.uasp.streaming.mdm.enrichment.itest.action.{CheckAction, CheckerOfCrossLinkMdm}
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.common.Finisheable
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.dao.DataGeneratorDao
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.entity.Config
-import ru.vtb.uasp.streaming.mdm.enrichment.itest.factory.ConsumerServiceFactory
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.CommonObject.{encoderUaspDto, genericDatumWriterUaspDto}
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.SendMdmCrossLinksScenarioBuilder._
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.TestEnrichProperty.globalIdEnrichPropertyTest
-import ru.vtb.uasp.streaming.mdm.enrichment.itest.service.{ConsumerService, IdConvertorService}
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.service.IdConvertorService
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.utils.IdsListGenerator.userIdLocal
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.utils.KafkaPropertiesUtil
 
 import scala.math.abs
 
 class SendMdmCrossLinksScenarioBuilder(val countUsers: Int, val config: Config) extends Finisheable {
-  val consumerMdmStatusDto: ConsumerService[Array[Byte], Array[Byte], String, UaspDto, Seq[UaspDto]] = ConsumerServiceFactory.getMdmStatusConsumerService(config,
-    KafkaPropertiesUtil.getConsumerKafkaProperties(config, config.groupIdCrossLinkStatusMdm))
 
-  def isFinished: Boolean = consumerMdmStatusDto.isFinished
+  def isFinished: Boolean = true
 
   def getSendMdmCrossLinksScenario: ScenarioBuilder = {
     val sendMdmCrossLinksScenario =
@@ -64,10 +60,7 @@ class SendMdmCrossLinksScenarioBuilder(val countUsers: Int, val config: Config) 
 //          session.set(clusterСrossLinkUaspDtoSessionName, clusterCrossLink)
 //        })
 //        .exec(new CheckAction("CheckerOfCrossLinkMdm", new CheckerOfCrossLinkMdm()))
-        .exec(session => {
-          consumerMdmStatusDto.finish()
-          session
-        })
+
 
     sendMdmCrossLinksScenario
   }
--- src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/scenario/SendWay4ScenarioBuilder.scala
+++ src/test/scala/ru/vtb/uasp/streaming/mdm/enrichment/itest/scenario/SendWay4ScenarioBuilder.scala
@@ -11,11 +11,10 @@ import ru.vtb.uasp.mdm.enrichment.service.dto.EnrichmentUaspWithError
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.common.Finisheable
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.dao.DataGeneratorDao
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.entity.Config
-import ru.vtb.uasp.streaming.mdm.enrichment.itest.factory.ConsumerServiceFactory
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.CommonObject.{encoderUaspDto, genericDatumWriterUaspDto}
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.SendWay4ScenarioBuilder._
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.scenario.TestEnrichProperty.{globalIdEnrichPropertyTest, hypothecProperty}
-import ru.vtb.uasp.streaming.mdm.enrichment.itest.service.{ConsumerService, IdConvertorService}
+import ru.vtb.uasp.streaming.mdm.enrichment.itest.service.{ IdConvertorService}
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.utils.IdsListGenerator.userIdLocal
 import ru.vtb.uasp.streaming.mdm.enrichment.itest.utils.KafkaPropertiesUtil
 
@@ -26,8 +25,6 @@ class SendWay4ScenarioBuilder(val countUsers: Int,
                              ) {
 
   val countMessages: Int = countUsers * countTransactions
-  val consumerEnrichmentDto: ConsumerService[Array[Byte], Array[Byte], String, UaspDto, Map[String, UaspDto]] = ConsumerServiceFactory.getEnrichmentConsumerService(config,
-    KafkaPropertiesUtil.getConsumerKafkaProperties(config, config.groupIdWay4))
   private val logger = LoggerFactory.getLogger(getClass)
 
   def getSendWay4Scenario: ScenarioBuilder = {
@@ -83,10 +80,7 @@ class SendWay4ScenarioBuilder(val countUsers: Int,
               new CheckAction("Compare_result&expectedResult_CheckerOfEnrichmentWay4", new CheckerOfEnrichmentWay4(consumerEnrichmentDto))
               session
             })*/
-      .exec(session => {
-        consumerEnrichmentDto.finish()
-        session
-      })
+
 
     sendWay4Scenario
   }
